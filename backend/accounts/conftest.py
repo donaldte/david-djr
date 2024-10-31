ae925d30ac8f4731d90fb69c5fa8197099adc0e5
Tchuindjang <paulnicolas519@gmail.com>
2024-10-31 08:32:47 +0100

    feat

---
+++ backend/accounts/conftest.py
@@ -0,0 +1,16 @@
+import pytest 
+from accounts.models import CustomUser
+from products.models import Product
+from rest_framework.test import APIClient
+
+
+@pytest.fixture
+def api_client() -> APIClient:
+    yield APIClient()
+
+
+@pytest.fixture
+def user() -> CustomUser:
+    return CustomUser   .objects.create_user(email='paulnicolas519@gmail.com', password='12345', otp='2345')
+
+
